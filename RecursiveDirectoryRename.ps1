32c3ad8c0de58899d181b0cb6a22a65fc499a89e
Aaron.Olds <aaron.olds@gmail.com>
2014-09-19 10:10:38 -0400

    Added Powershell script

---
+++ RecursiveDirectoryRename.ps1
@@ -0,0 +1,25 @@
+﻿$startDate = Get-Date;
+$basePath = "[ReplaceMeWithTheStartingPath]"
+Write-Host $startDate;
+$dirs = gci -Path $basePath | Where-Object {$_.Attributes -eq 'Directory'}
+foreach ( $dir in $dirs ) {
+    Write-Host $dir
+    $newPath = $basePath + $dir
+    Set-Location $newPath
+
+    $dirPurpleSummaries = gci -Path $newPath -Recurse | Where-Object {$_.Attributes -eq 'Directory' -and $_.Name -eq '[FolderNameToBeReplaced]'}
+    foreach ( $dirPurpleSummary in $dirPurpleSummaries ) {
+        Write-Host $dirPurpleSummary
+        $item = Get-Item $dirPurpleSummary.FullName
+        $oldpath = $($item.fullname)
+        $oldname = $($item.fullname | split-path -leaf)
+        $newname = $($oldname) -replace "[FolderNameToBeReplaced]","[NewFolderName]"
+        #write-host Checking Path: $oldpath
+        if (!$oldname.contains($newname)) {
+            Write-Host Renaming $oldname to $newname;
+            Rename-Item -Path $oldpath -NewName $newname
+        }
+    }
+}
+$endDate = Get-Date;
+Write-Host $endDate
