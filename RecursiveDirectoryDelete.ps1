9eafb0ae6cfc6298f49d3de2784a7440765f41a2
Aaron.Olds <aaron.olds@gmail.com>
2014-09-25 08:44:31 -0400

    Added  recursive directory move
    Needed a new script to move files out of one folder into a new folder

---
+++ RecursiveDirectoryDelete.ps1
@@ -0,0 +1,16 @@
+﻿$startDate = Get-Date;
+$basePath = "\\dmps_sql12_fs\DMPS\DMPSReportLibrary\"
+Write-Host $startDate;
+$dirs = gci -Path $basePath | Where-Object {$_.Attributes -eq 'Directory'}
+foreach ( $dir in $dirs ) {
+    #Write-Host $dir
+    $newPath = $basePath + $dir 
+    $badPath = $newPath + "\2014\Purple Summary"
+    $badPathExists = Test-Path -Path $badPath;
+    if($badPathExists -eq "true"){
+        Write-Host "removing: " $badPath
+        Remove-Item -Path $badPath -Force -Recurse
+    }
+}
+$endDate = Get-Date;
+Write-Host $endDate
---
+++ RecursiveDirectoryMove.ps1
@@ -0,0 +1,17 @@
+﻿$startDate = Get-Date;
+$basePath = "\\dmps_sql12_fs\DMPS\DMPSReportLibrary\"
+Write-Host $startDate;
+$dirs = gci -Path $basePath | Where-Object {$_.Attributes -eq 'Directory'}
+foreach ( $dir in $dirs ) {
+    #Write-Host $dir
+    $newPath = $basePath + $dir 
+    $badPath = $newPath + "\2014\Purple Summary\Quarterly FICA SUI FUTA\3rd Quarter"
+    $goodPath = $newPath + "\2014\Quarterly Summary\Quarterly FICA SUI FUTA\3rd Quarter"
+    $badPathExists = Test-Path -Path $badPath;
+    if($badPathExists -eq "true"){
+        Write-Host "move: " $badPath " to " $goodPath;
+        move-item -path $badPath -destination $goodPath
+    }
+}
+$endDate = Get-Date;
+Write-Host $endDate
